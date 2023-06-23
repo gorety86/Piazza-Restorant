91e99813ea2442364de5d92e2da780503ee78f4b
gorety <wagutugorety@gmail.com>
2023-06-23 17:18:51 +0300

    made changes in restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+    has_many :restaurant_pizzas, dependent: :destroy
+    has_many :pizzas, through: :restaurant_pizzas
+
+    validates :address, presence: true
+    validates :name, presence: true
 end
