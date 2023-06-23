e0cbcab934c18d3d80edc9369ddab2ce0b09d29b
gorety <wagutugorety@gmail.com>
2023-06-23 22:07:08 +0300

    retaurant.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,4 +13,14 @@ class RestaurantsController < ApplicationController
           render json: { error: "Restaurant not found" }, status: :not_found
         end
     end
+
+    def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+          restaurant.destroy
+          head :no_content
+        else
+          render json: { error: "Restaurant not found" }, status: :not_found
+        end
+    end
 end
