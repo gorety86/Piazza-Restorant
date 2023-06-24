238c0e382dfb0081c964a31e337974b2647cc10d
gorety <wagutugorety@gmail.com>
2023-06-24 12:10:34 +0300

    made changes in restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,31 +1,40 @@
 class RestaurantsController < ApplicationController
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants
+  end
 
-    def index
-        restaurants = Restaurant.all
-        render json: restaurants
-    end
+  def show
+    restaurant = find_restaurant
+    render_restaurant(restaurant)
+  end
 
-    def show
-        restaurant = Restaurant.find_by(id: params[:id])
-        if restaurant
-          render json: restaurant
-        else
-          render json: { error: "Restaurant not found" }
-        end
+  def destroy
+    restaurant = find_restaurant
+    if restaurant
+      restaurant.destroy
+      head :no_content
+    else
+      render_restaurant_not_found
     end
+  end
 
-    def destroy
-        restaurant = Restaurant.find_by(id: params[:id])
-        if restaurant
-          restaurant.destroy
-          head :no_content
-        else
-          render json: { error: "Restaurant not found" }
-        end
-    end
+  private
 
-    private
-    def restaurant_params
-      params.permit()
+  def find_restaurant
+    Restaurant.find_by(id: params[:id])
+  end
+
+  
+  def render_restaurant(restaurant)
+    if restaurant
+      render json: restaurant
+    else
+      render_restaurant_not_found
     end
+  end
+
+  def render_restaurant_not_found
+    render json: { error: "Restaurant not found!" }, status: :not_found
+  end
 end
