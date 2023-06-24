9fe5f71912da2c474686b9a0765559f504a75808
gorety <wagutugorety@gmail.com>
2023-06-24 11:25:10 +0300

    restaurantpizzas cotrollers.rb

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,3 +1,15 @@
 class RestaurantPizzasController < ApplicationController
-  
+  def index
+    restaurantpizzas = RestaurantPizza.all
+    render json: restaurantpizzas
+  end
+
+  def update
+    restaurantpizzas = RestaurantPizza.find(params[:id])
+    if restaurantpizzas.update(post_params)
+      render json: restaurantpizzas
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
